6bc98a5e49f9c755a3ada1f90183147c476807dc
Anastasia <anti2100@yandex.ru>
2019-04-20 09:53:19 +0000

    Update test_views.py

--- test/test_views.py
+++ test/test_views.py
@@ -32,15 +32,4 @@ def test_handle_pictures_request_unknown_command(test_client):
     with app.test_request_context():
         response = test_client.put('/api/pictures/')
         assert response.status_code == 405
-#
-#
-# def test_handle_user_request_get(mocker, test_client):
-# mocker.patch('api.models.User.get_user', return_value=None)
-# with app.test_request_context():
-# response = test_client.get('/api/users/123')
-# assert response.status_code == 204
-# mocker.patch('api.models.User.get_user', return_value=json.dumps({}))
-# with app.test_request_context():
-# response = test_client.get('/api/users/123')
-# assert response.status_code == 200
-# assert response.data.decode() == '{}'
+        
